84c1446f09d9fc7af4cc90f743f2a3e26dddac85
apurvkolte <apurvkolte10@gmail.com>
2024-02-13 11:00:58 +0530

    ReactJS

--- date.js
+++ date.js
@@ -14,3 +14,18 @@ export default date;
 //> "1/24/2024 10:32:49"
 
 //new Date().toLocaleString()
+
+//sql  select DATE_FORMAT(NOW(),'%d/%m/%Y %H:%i:%s');
+
+
+
+
+var d = new Date("2023-01-22 00:00:00").toLocaleDateString('en-IN');
+// "22/01/2023"
+
+var d = new Date("2023-01-22 00:00:00").toLocaleString('en-IN')
+//"22/01/2023, 00:00:00"
+
+
+const date = new Date("2023-01-22 00:00:00");
+console.log(date.getDate() + '/' + date.getMonth() + 1 + '/' + date.getFullYear());
